1a87ff61772297ac9b1a17453c91546b4840dee1
TBushkiev <bushkiev08@gmail.com>
2023-07-07 17:36:55 +0200

    Fix datetime issue

--- bot/parsers/vestikalm.py
+++ bot/parsers/vestikalm.py
@@ -1,10 +1,24 @@
 import datetime
 import requests
 from bs4 import BeautifulSoup
-import locale
 
 
 class Vesti:
+    month_dict = {
+        'января': '1',
+        'февраля': '2',
+        'марта': '3',
+        'апреля': '4',
+        'мая': '5',
+        'июня': '6',
+        'июля': '7',
+        'августа': '8',
+        'сентября': '9',
+        'октября': '10',
+        'ноября': '11',
+        'декабря': '12'
+    }
+
     def __init__(self):
         self.website_link = 'https://vesti-kalmykia.ru/news'
 
@@ -14,7 +28,7 @@ class Vesti:
         Получает html код страницы, возвращает заголовок новости
         """
         title = html_code.find("h1", class_="item-title uk-margin")
-        return title.text.strip()
+        return title.text.strip().replace('\xa0', ' ')
 
     @staticmethod
     def _get_news_text(html_code) -> str:
@@ -25,7 +39,7 @@ class Vesti:
         if text is None:
             text = html_code.find("h1", class_="item-title uk-margin")
             return str(text)
-        return text.text.strip()
+        return text.text.strip().replace('\xa0', ' ')
 
     @staticmethod
     def _get_news_date(html_code) -> str:
@@ -33,12 +47,17 @@ class Vesti:
         Получает html код страницы, возвращает заголовок новости
         """
         news_time: str = html_code.find("div", class_="uk-width-expand@m").find("div", class_="item-date").text
-        locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
-        date: str = str(datetime.datetime.strptime(news_time, '%d.%m.%Y %H:%M')) # Здесь Python ругался, если нет второго str
-        date, time = date.split(',')
-        date += ' ' + str(datetime.datetime.now().year)
-        date = date + time
-        return str(datetime.datetime.strptime(date, u'%d.%m.%Y %H:%M')) # И здесь
+
+        date, time = news_time.split(', ')
+
+        date = date.split()
+        date[1] = Vesti.month_dict[date[1]]
+        if len(date) == 2:
+            date.append(str(datetime.datetime.now().year))
+
+        date = ' '.join(date)
+        news_time = ', '.join([date, time])
+        return str(datetime.datetime.strptime(news_time, '%d %m %Y, %H:%M'))
 
     def get_news_data(self, url: str) -> (str, str, str):
         """
@@ -51,3 +70,12 @@ class Vesti:
         news_text = self._get_news_text(html_code)
         news_date = self._get_news_date(html_code)
         return news_title, news_text, news_date
+
+
+if __name__ == '__main__':
+    url = 'https://vesti-kalmykia.ru/news/zaversheno-rassledovanie-ugolovnogo-dela-v-otnoshenii-sotrudnika-celinnogo-res-obvinyaemogo-v-zloupotreblenii-sluzhebnymi-polnomochiyami'  # новость из текущего года
+    old_news_url = 'https://vesti-kalmykia.ru/news/v-zagorodnoj-rezidencii-basana-gorodovikova-sostoyalas-uborka'  # новость из прошлого года
+
+    parser = Vesti()
+    print(parser.get_news_data(url=url))
+    print(parser.get_news_data(url=old_news_url))
