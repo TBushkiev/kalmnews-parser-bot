977d14b558a9fafbd2958102a5cc59c3f504e673
TBushkiev <bushkiev08@gmail.com>
2023-07-16 19:07:19 +0200

    fix copypaste issues, add select query crud

--- bot/crud/news.py
+++ bot/crud/news.py
@@ -1,4 +1,7 @@
 from typing import Type
+
+from sqlalchemy import select
+
 from database.get_db import get_db
 from models.news import News
 
@@ -13,6 +16,11 @@ class CRUDNews:
         news = News(title=title, text=text, date=date, sentiment=sentiment, score=score)
         session.add(news)
         session.commit()
+
+    def get_by_id(self, news_id):
+        session = get_db()
+        query = select(self.model).where(self.model.id == news_id)
+        return session.execute(query).scalar()
 
 
 crud = CRUDNews(News)
--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,20 @@
 import datetime
-from crud.news import crud
+from crud.news import CRUDNews, crud as crud_news
 from telebot import TeleBot
 from telebot.types import Message
-from parsers.riakalm import parser as riakalm_parser
-from parsers.vestikalm import parser as vestikalm_parser
+from parsers.riakalm import parser as riakalm_parser, RiaKalm
+from parsers.vestikalm import parser as vestikalm_parser, Vesti
 from core.config import settings
-
+from models.news import News
 bot = TeleBot(settings.BOT_TOKEN)
+
+
+def get_data(url: str, parser: RiaKalm | Vesti, crud: CRUDNews) -> (str, str, str):
+    title, text, news_date = parser.get_news_data(url=url)
+
+    crud.add_new_item(title=title, text=text, date=news_date)
+
+    return title, text, news_date
 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -23,19 +31,20 @@ def add_news(message: Message):
     else:
         url = params[1]
         if 'riakalm' in url:
-            title, text, news_date = riakalm_parser.get_news_data(url=url)
+            title, text, news_date = get_data(url=url, parser=riakalm_parser, crud=crud_news)
 
-            crud.add_new_item(title=title, text=text, date=news_date)
-
-            bot.send_message(chat_id=message.chat.id, text=f'{title}\n{text}\n{news_date}')
-
-            bot.send_message(chat_id=message.chat.id, text=f'{title}\n{text}\n{news_date}')
         elif 'vesti-kalmykia' in url:
-            title, text, news_date = vestikalm_parser.get_news_data(url=url)
+            title, text, news_date = get_data(url=url, parser=vestikalm_parser, crud=crud_news)
 
-            crud.add_new_item(title=title, text=text, date=news_date)
+        bot.send_message(chat_id=message.chat.id, text=f'{title}\n\n{text}\n\n{news_date}')
 
-            bot.send_message(chat_id=message.chat.id, text=f'{title}\n{text}\n{news_date}')
+
+@bot.message_handler(commands=['get_by_id'])
+def get_news_by_id(message: Message):
+    params = message.text.split()
+    news_id = int(params[1])
+    news: News = crud_news.get_by_id(news_id)
+    bot.send_message(chat_id=message.chat.id, text=f'{news.title}\n\n{news.text}\n\n{news.date}')
 
 
 if __name__ == '__main__':
